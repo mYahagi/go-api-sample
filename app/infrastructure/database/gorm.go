01f47da727d474e330c5c53e877c6a98c802bc0f
myahagi <myahagi@coubic.com>
2020-07-06 08:26:46 +0900

    ディレクトリ構成変更

--- app/infrastructure/database/gorm.go
+++ app/infrastructure/database/gorm.go
@@ -1,18 +1,19 @@
 package database
+
 import (
-   "github.com/jinzhu/gorm"
-   _ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func GormConnect() *gorm.DB {
-	DBMS     := "mysql"
-	USER     := "root"
-	PASS     := "pass"
+	DBMS := "mysql"
+	USER := "root"
+	PASS := "pass"
 	PROTOCOL := "tcp(127.0.0.1:3306)"
-	DBNAME   := "go_sample"
+	DBNAME := "go_sample"
 
-	CONNECT := USER+":"+PASS+"@"+PROTOCOL+"/"+DBNAME
-	db,err := gorm.Open(DBMS, CONNECT)
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME
+	db, err := gorm.Open(DBMS, CONNECT)
 
 	if err != nil {
 		panic(err.Error())

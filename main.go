3b61e6e3b5a1de31896382f4797d24605c11c894
myahagi <myahagi@coubic.com>
2020-07-05 19:45:04 +0900

    fix

--- main.go
+++ main.go
@@ -1,20 +1,19 @@
 package main
 
 import (
-    "./src/app/infrastructure/database"
-    "github.com/gin-gonic/gin"
-    //"github.com/mYahagi/src/app/infrastructure/database"
+	"./src/app/infrastructure/database"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    r := gin.Default()
-    db := database.GormConnect()
+	r := gin.Default()
+	db := database.GormConnect()
 
-    defer db.Close()
-    r.GET("/ping", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "pong",
-        })
-    })
-    r.Run() // listen and serve on 0.0.0.0:8080
+	defer db.Close()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }

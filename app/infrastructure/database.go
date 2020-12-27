71963d8b844214a95cfee9c371d0b1bc689155b8
mYahagi <myahagi@hey.jp>
2020-12-27 20:29:17 +0900

    db設定外だし

---
+++ app/infrastructure/database.go
@@ -0,0 +1,27 @@
+package infrastructure
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"time"
+)
+
+func Connect() *gorm.DB {
+	DBMS := "mysql"
+	USER := "root"
+	PASS := "pass"
+	PROTOCOL := "tcp(127.0.0.1:3306)"
+	DBNAME := "go_sample"
+
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME
+	db, err := gorm.Open(DBMS, CONNECT)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	db.DB().SetMaxIdleConns(10)
+	db.DB().SetMaxOpenConns(100)
+	db.DB().SetConnMaxLifetime(5 * time.Minute)
+
+	return db
+}
--- main.go
+++ main.go
@@ -2,35 +2,13 @@ package main
 
 import (
 	"fmt"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
-	"time"
+	"github.com/mYahagi/go-api-sample/app/infrastructure"
 )
 
 func main() {
 	fmt.Println("hello")
-	db := Connect()
+	db := infrastructure.Connect()
 
 	db.Close()
 	fmt.Println("bye")
-}
-
-func Connect() *gorm.DB {
-	DBMS := "mysql"
-	USER := "root"
-	PASS := "pass"
-	PROTOCOL := "tcp(127.0.0.1:3306)"
-	DBNAME := "go_sample"
-
-	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME
-	db, err := gorm.Open(DBMS, CONNECT)
-
-	db.DB().SetMaxIdleConns(10)
-	db.DB().SetMaxOpenConns(100)
-	db.DB().SetConnMaxLifetime(5 * time.Minute)
-
-	if err != nil {
-		panic(err.Error())
-	}
-	return db
 }

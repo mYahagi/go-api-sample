e3a6bd6d44acc67f236649f18d9d4975d5bf3ae6
mYahagi <myahagi@hey.jp>
2021-01-23 01:00:52 +0900

    それっぽい処理の雛形

--- app/controller/prices_controller.go
+++ app/controller/prices_controller.go
@@ -4,19 +4,22 @@ import (
 	"fmt"
 	"net/http"
 
-	usecase2 "github.com/mYahagi/go-api-sample/app/domain/usecase"
+	"github.com/mYahagi/go-api-sample/app/domain/usecase"
 )
 
 type PriceController struct{}
 
 func (ctrl PriceController) Show(res http.ResponseWriter, req *http.Request) {
-	endStation := req.FormValue("end")
-	usecase := usecase2.NewGetPrice(endStation)
-	price, err := usecase.Execute()
+	// TODO bindingした値をstring型の引数に渡すと死ぬのでちゃんとした方法探してくる
+	//var endStation string
+	//endStation = req.FormValue("end")
 
+	u := usecase.NewGetPrice("大阪駅")
+	price, err := u.Execute()
 	if err != nil {
 		fmt.Fprint(res, "error")
 	}
+
 	fmt.Fprint(res, price)
 }
 

90922e53d0eaaf9904a4fe91e7db6ca28f4122e8
mYahagi <myahagi@hey.jp>
2021-01-23 03:50:57 +0900

    ポインタにしておく

--- app/domain/repository/basic_fare.go
+++ app/domain/repository/basic_fare.go
@@ -4,5 +4,5 @@ import "github.com/mYahagi/go-api-sample/app/infrastructure/model"
 
 type IBasicFareRepository interface {
 	// TODO 適切なentity作ったらそれを返すようにする
-	Find(endStation string) model.BasicFare
+	Find(endStation string) *model.BasicFare
 }

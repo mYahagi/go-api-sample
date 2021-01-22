b21d61466c7024f5b4ef737d8660c5caa93e95b7
mYahagi <myahagi@hey.jp>
2021-01-23 03:53:36 +0900

    errorも返す

--- app/domain/repository/express_charge.go
+++ app/domain/repository/express_charge.go
@@ -4,5 +4,5 @@ import "github.com/mYahagi/go-api-sample/app/infrastructure/model"
 
 type IExpressChargeRepository interface {
 	// TODO 適切なentity作ったらそれを返すようにする
-	Find() *model.ExpressCharge
+	Find() (*model.ExpressCharge, error)
 }

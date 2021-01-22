b2bd529e277b01dee049b6515106db9e4a6e36d5
mYahagi <myahagi@hey.jp>
2021-01-23 03:16:02 +0900

    パラメータから終着駅を取得する

--- app/controller/prices_controller.go
+++ app/controller/prices_controller.go
@@ -10,13 +10,13 @@ import (
 type PriceController struct{}
 
 func (ctrl PriceController) Show(res http.ResponseWriter, req *http.Request) {
-	// TODO bindingした値をstring型の引数に渡すと死ぬのでちゃんとした方法探してくる
-	//var endStation string
-	//endStation = req.FormValue("end")
+	start := "東京駅"
+	end := req.URL.Query().Get("end")
 
-	u := usecase.NewGetPrice("大阪駅")
+	u := usecase.NewGetPrice(start, end)
 	price, err := u.Execute()
 	if err != nil {
+		res.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprint(res, "error")
 	}
 
